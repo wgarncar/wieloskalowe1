6fb9679e14b80ea3955bc9d3c142cbf9816fe0b7
wgarncar <garncarz.wojtek@gmail.com>
2019-04-28 18:30:41 +0200

    1 part

---
+++ projekt1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace projekt1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(562, 416);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(612, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(612, 210);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(612, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(609, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "rule";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(609, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "width";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ projekt1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projekt1
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap DrawArea;
+        Graphics g;
+        Pen pen;
+        Brush brush;
+        int y = 0;
+        int rectSize = 10;
+        int count = 500;
+        int rule;
+        string binaryRule;
+
+        bool start = true;
+
+        List<List<bool>> states;
+        public Form1()
+        {
+            InitializeComponent();
+
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = DrawArea;
+
+            states = new List<List<bool>>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = Graphics.FromImage(DrawArea);
+            pen = new Pen(Brushes.Black);
+            brush = new SolidBrush(Color.Black);
+
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 10;
+        }
+
+        private void calculate()
+        {
+            List<bool> newState = Enumerable.Repeat(false, count).ToList();
+            int previousState = states.Count - 1;
+
+            for (int i = 0; i < newState.Count; i++)
+            {
+                string binary = "";
+
+                int previous = i - 1;
+                if(previous < 0)
+                {
+                    previous = newState.Count - 1;
+                }
+
+                int next = i + 1;
+                if(next >= newState.Count)
+                {
+                    next = 0;
+                }
+
+                binary += states[previousState][previous] ? "1" : "0";
+                binary += states[previousState][i] ? "1" : "0";
+                binary += states[previousState][next] ? "1" : "0";
+
+                int index = Convert.ToInt32(binary, 2);
+
+                if(binaryRule.Length > index && binaryRule[binaryRule.Length - index - 1] == '1')
+                {
+                    newState[i] = true;
+                }
+            }
+
+            states.Add(newState);
+        }
+
+        private void draw()
+        {
+            int x = 0;
+            foreach(bool state in states[states.Count - 1])
+            {
+                if (state)
+                {
+                    g.FillRectangle(brush, x, y, rectSize, rectSize);
+                    g.Flush();
+                }
+                x += rectSize;
+            }
+            y += rectSize;
+            pictureBox1.Image = DrawArea;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (start == false)
+            {
+
+                DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+                pictureBox1.Image = DrawArea;
+                g.Clear(Color.White);
+                g = Graphics.FromImage(DrawArea);
+
+                states.Clear();
+                y = 0;
+                
+                /*timer1 = new Timer();
+                timer1.Tick += new EventHandler(timer1_Tick);
+                timer1.Interval = 10;*/
+            }
+            start = false;
+
+            if (!Int32.TryParse(textBox2.Text, out rule))
+            {
+                rule = 90;
+            }
+
+            binaryRule = Convert.ToString(rule, 2);
+
+            if(!Int32.TryParse(textBox1.Text, out count))
+            {
+                count = pictureBox1.Width;
+            }
+
+            rectSize = pictureBox1.Size.Width / count;
+
+            List<bool> initialState = Enumerable.Repeat(false, count).ToList();
+            initialState[count / 2] = true;
+            states.Add(initialState);
+            draw();
+            
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            calculate();
+            draw();
+        }
+    }
+}
